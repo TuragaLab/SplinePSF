ea787a3aaa72517c99302af036cd116603a9ef78
Lucas-Rapahel Müller <gitdev@lrm.photo>
2021-03-28 14:41:19 +0200

    Bump version: 0.10.0 → 0.10.1dev0

--- python/setup.py
+++ python/setup.py
@@ -64,7 +64,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='spline',
-    version='0.10.0',
+    version='0.10.1dev0',
     packages=setuptools.find_packages(),
     ext_modules=[CMakeExtension('spline', '../cpp_cuda_c')],
     cmdclass=dict(build_ext=CMakeBuild),
